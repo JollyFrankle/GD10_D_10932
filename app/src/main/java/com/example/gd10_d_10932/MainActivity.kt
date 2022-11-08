eab3886a85bd059ef0ff238f2790eae9834b1dfd
JollyFrankle <jollyfrankle3@gmail.com>
2022-11-08 09:57:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd10_d_10932/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.gd10_d_10932
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.example.gd10_d_10932.databinding.ActivityMainBinding
+import com.example.gd10_d_10932.fragment.DataMahasiswaFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showDataFragment()
+        binding.txtCari.setOnKeyListener(View.OnKeyListener{ _, keyCode, event ->
+            if(keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                showDataFragment()
+                return@OnKeyListener true
+            }
+            false
+        })
+        binding.btnAdd.setOnClickListener{
+            startActivity(
+                Intent(this, FormAddMahasiswaActivity::class.java)
+            )
+        }
+    }
+
+    fun showDataFragment() {
+        val mFragmentTransaction = supportFragmentManager.beginTransaction()
+        val mFragment = DataMahasiswaFragment()
+        val textCari = binding.txtCari.text
+        val mBundle = Bundle()
+        mBundle.putString("cari", textCari.toString())
+        mFragment.arguments = mBundle
+        mFragmentTransaction.replace(R.id.fl_data, mFragment).commit()
+
+        // Alert
+//        AlertDialog.Builder(this).apply {
+//            setTitle("Informasi")
+//            setMessage("OK sejauh ini")
+//            setPositiveButton("OK"){dialog, which ->
+//                dialog.dismiss()
+//            }
+//        }.show()
+
+    }
+}
